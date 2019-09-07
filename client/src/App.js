fc8425b4c3f48d377215e85a8f2b3fe58d7cf737
bpawanchand <bpawanchand@gmail.com>
2019-09-07 20:12:35 +0200

    IntialDelta Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import './App.css';
+import AppNavbar from '../src/components/layout/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <AppNavbar></AppNavbar>
     </div>
   );
-}
+};
 
 export default App;
