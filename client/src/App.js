18a1dd180d654d6f7ac024733651476339a48ddd
bpawanchand <bpawanchand@gmail.com>
2019-09-11 17:45:10 +0200

    Store Settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppNavbar from '../src/components/layout/Navbar';
+import RegisterForm from '../src/components/register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+//  redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 const App = () => {
   return (
-    <div className='container-fluid'>
-      <AppNavbar></AppNavbar>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <AppNavbar />
+          <section className="container">
+            <RegisterForm />
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
