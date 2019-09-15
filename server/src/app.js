45cbfd5dd3dfbf1ab3f61ffc1a746f19502410dc
bpawanchand <bpawanchand@gmail.com>
2019-09-15 02:41:08 +0200

    Production Build 1

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Application is running...');
-});
 
 app.use(express.json({ extended: false }));
 
 app.use('/user', require('../routes/user'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server Running on Port ${port}`);
 });
